d945da5fcd37ec2ca7f035fa697bdc1af7fbb21d
若鱼不是鱼 <21231270@bjtu.edu.cn>
2022-12-22 16:16:00 +0800

    二叉树的各项基本操作

--- test2022_12_21/test2022_12_21/test.cpp
+++ test2022_12_21/test2022_12_21/test.cpp
@@ -154,56 +154,135 @@ typedef struct Quene
 {
 	int front;
 	int rear;
-	int data[MAXSIZE];
+	LinkTree data[MAXSIZE];
 }Quene;
 //初始化队列
 void Init_Quene(Quene& Q)
 {
 	Q.front = Q.rear = 0;
 }
-void Push_Quene()
+//入队操作
+void Push_Quene(Quene & Q,LinkTree T)
 {
-
+	if ((Q.rear + 1) % MAXSIZE == Q.front)
+	{
+		cout << "队列已满，无法入队" << endl;
+		return;
+	}
+	Q.data[Q.rear] = T;
+	Q.rear = (Q.rear + 1) % MAXSIZE;
+}
+//出队操作
+void Pop_Quene(Quene& Q, LinkTree& e)
+{
+	if (Q.front == Q.rear)
+	{
+		cout << "队空，无法出队" << endl;
+		e = NULL;
+		return;
+	}
+	e = Q.data[Q.front];
+	Q.front = (Q.front + 1) % MAXSIZE;
 }
 //层次遍历二叉树
 void InOrderTraverse(LinkTree T)
 {
+	if (T == NULL)
+	{
+		cout << "该树为空树" << endl;
+		return;
+	}
 	Quene Q;
 	Init_Quene(Q);
-
-
+	Push_Quene(Q, T);
+	while (Q.front != Q.rear)
+	{
+		LinkTree e = NULL;
+		Pop_Quene(Q, e);
+		cout << e->data << ' ';
+		if (e->lchild != NULL)
+		{
+			Push_Quene(Q, e->lchild);
+		}
+		if (e->rchild != NULL)
+		{
+			Push_Quene(Q, e->rchild);
+		}
+	}
 }
 //复制二叉树
-
+void Copy_Tree(LinkTree T, LinkTree& NewT)
+{
+	if (T == NULL)
+	{
+		NewT = NULL;
+		return;
+	}
+	NewT = new TreeNode;
+	NewT->data = T->data;
+	Copy_Tree(T->lchild, NewT->lchild);
+	Copy_Tree(T->rchild, NewT->rchild);
+}
 //计算节点数
-
+int numNodes(LinkTree T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	else
+		return numNodes(T->lchild) + numNodes(T->rchild) + 1;
+}
 //计算叶子节点
-
+int numLeaves(LinkTree T)
+{
+	if (T == NULL)
+		return 0;
+	if (T->lchild == NULL && T->rchild == NULL)
+	{
+		return 1;
+	}
+	else
+		return numLeaves(T->lchild) + numLeaves(T->rchild);
+}
 
 int main()
 {
 	LinkTree T;
 	//创建二叉树
 	creatTree(T);
+	
 	//前序遍历二叉树
 	cout << "前序遍历二叉树" << endl;
 	PreOrderTraverse(T);
 	cout << endl;
+
 	//中序递归遍历
 	cout << "中序遍历二叉树" << endl;
 	MidOrderTraverse(T);
 	cout << endl;
+
 	//中序非递归遍历
 	cout << "中序非递归遍历二叉树" << endl;
 	Mid01OrderTraverse(T);
 	cout << endl;
+
 	//后序递归遍历
 	cout << "后序遍历二叉树" << endl;
 	PostOrderTraverse(T);
 	cout << endl;
+
 	//层次遍历二叉树遍历
 	cout << "层次遍历二叉树" << endl;
 	InOrderTraverse(T);
 	cout << endl;
+
+	//拷贝二叉树
+	LinkTree NewT;
+	Copy_Tree(T, NewT);
+	//计算二叉树的节点
+	cout << "该树的节点个数为：" << numNodes(T) << endl;
+	//计算二叉树的叶子节点
+	cout << "该树的叶子节点个数为：" << numLeaves(T) << endl;
 	return 0;
 }
