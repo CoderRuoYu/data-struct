bef4f131e7611321bb789bd3abfef739d7d1ecfa
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-01-09 14:29:26 +0800

    test_2023_1_9

--- 二叉排序树/二叉排序树/test.cpp
+++ 二叉排序树/二叉排序树/test.cpp
@@ -6,12 +6,82 @@ typedef struct Tree
 	int data;
 	struct Tree* lchild, * rchild;
 }Tree, * LinkTree;
-void init_tree(LinkTree T)
+void init_tree(LinkTree& T)
 {
-
+	T = NULL;
+}
+void insert_tree(LinkTree& T, int e)
+{
+	if (!T)
+	{
+		LinkTree p = new Tree;
+		p->data = e;
+		p->lchild = NULL;
+		p->rchild = NULL;
+		T = p;
+	}
+	else if (e <= T->data)
+	{
+		insert_tree(T->lchild, e);
+	}
+	else
+	{
+		insert_tree(T->rchild, e);
+	}
+}
+void creat_tree(LinkTree& T)
+{
+	int n = 0;
+	cout << "请输入您要创建二叉排序树的节点个数:";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int e = 0;
+		cin >> e;
+		insert_tree(T, e);
+	}
+}
+void mid_traverse(LinkTree& T)
+{
+	if (!T)return;
+	mid_traverse(T->lchild);
+	cout << T->data << ' ';
+	mid_traverse(T->rchild);
+}
+LinkTree search_tree(LinkTree& T,int key)
+{
+	if (!T)return T;
+	if (T->data == key)return T;
+	else if (key > T->data)return search_tree(T->rchild, key);
+	else return search_tree(T->lchild, key);
+}
+LinkTree search_tree1(LinkTree& T, int key)
+{
+	if (!T)return T;
+	LinkTree p = T;
+	while (p != NULL && p->data != key)
+	{
+		if(T->data>=key)T=T->
+	}
+}
+void test(LinkTree& T)
+{
+	//创建二叉排序树
+	creat_tree(T);
+	//中序遍历二叉排序树
+	mid_traverse(T);
+	cout << endl;
+	int key = 0;
+	cout << "请输入您要查找的数值";
+	cin >> key;
+	LinkTree ret = search_tree(T, key);
+	if (!ret)cout << "在二叉排序树中不存在这个数" << endl;
+	else cout << "找到了这个数，这个数是:" << ret->data << endl;
 }
 int main()
 {
 	LinkTree T;
+	init_tree(T);
+	test(T);
 	return 0;
 }
