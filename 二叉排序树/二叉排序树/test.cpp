e65d892156b35a5854acdc98a5558583536c935d
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-01-12 10:01:47 +0800

    二叉排序树

--- 二叉排序树/二叉排序树/test.cpp
+++ 二叉排序树/二叉排序树/test.cpp
@@ -6,6 +6,7 @@ typedef struct Tree
 	int data;
 	struct Tree* lchild, * rchild;
 }Tree, * LinkTree;
+//初始化二叉树
 void init_tree(LinkTree& T)
 {
 	T = NULL;
@@ -29,25 +30,7 @@ void insert_tree(LinkTree& T, int e)
 		insert_tree(T->rchild, e);
 	}
 }
-void insert_tree1(LinkTree& T, int e)
-{
-	if (!T)
-	{
-		LinkTree p = new Tree;
-		p->data = e;
-		p->lchild = NULL;
-		p->rchild = NULL;
-		T = p;
-	}
-	else if (e <= T->data)
-	{
-		insert_tree(T->lchild, e);
-	}
-	else
-	{
-		insert_tree(T->rchild, e);
-	}
-}
+//创建二叉排序树
 void creat_tree(LinkTree& T)
 {
 	int n = 0;
@@ -60,6 +43,7 @@ void creat_tree(LinkTree& T)
 		insert_tree(T, e);
 	}
 }
+//中序遍历二叉排序树
 void mid_traverse(LinkTree& T)
 {
 	if (!T)return;
@@ -91,7 +75,95 @@ LinkTree search_tree1(LinkTree T, int key)
 	}
 	return p;
 }
-//void delete_node(LinkTree &T)
+//删除二叉排序树
+void delete_node(LinkTree T, int key)
+{
+	if (T == NULL)return;
+	LinkTree par;
+	LinkTree son = T;
+	while (1)
+	{
+		if (son == NULL)return;
+		par = son;
+		if (key < son->data)
+		{
+			son = son->lchild;
+		}
+		else if (key > son->data)
+		{
+			son = son->rchild;
+		}
+		if (son->data == key)
+		{
+			break;
+		}
+	}
+	if (!son->lchild && !son->rchild)
+	{
+		if (par->lchild == son)
+			par->lchild = NULL;
+		else if (par->rchild == son)
+			par->rchild = NULL;
+		return;
+	}
+	else if (!son->lchild && son->rchild)
+	{
+		if (par->lchild == son)
+		{
+			par->lchild = son->rchild;
+			delete son;
+		}
+		else if (par->rchild == son)
+		{
+			par->rchild = son->rchild;
+			delete son;
+		}
+		return;
+	}
+	else if (son->lchild && !son->rchild)
+	{
+		if (par->lchild == son)
+		{
+			par->lchild = son->lchild;
+			delete son;
+		}
+		else if (par->rchild == son)
+		{
+			par->rchild = son->lchild;
+			delete son;
+		}
+		return;
+	}
+	else
+	{
+		LinkTree tem = son;
+
+		par = son;
+		son = son->rchild;
+		while (1)
+		{
+			if (son->lchild == NULL)
+			{
+				break;
+			}
+			par = son;
+			son = son->lchild;
+		}
+		if (par->rchild == son)
+		{
+			par->rchild = son->rchild;
+			delete son;
+			return;
+		}
+		else
+		{
+			tem->data = son->data;
+			par->lchild = NULL;
+			delete son;
+			return;
+		}
+	}
+}
 void test(LinkTree& T)
 {
 	//创建二叉排序树
@@ -105,6 +177,13 @@ void test(LinkTree& T)
 	LinkTree ret = search_tree1(T, key);
 	if (!ret)cout << "在二叉排序树中不存在这个数。" << endl;
 	else cout << "找到了这个数，这个数是:" << ret->data << endl;
+	cout << endl;
+	int e = 0;
+	cout << "请输入您要删除的数字:";
+	cin >> e;
+	delete_node(T, e);
+	//中序遍历二叉排序树
+	mid_traverse(T);
 	cout << endl;
 }
 
